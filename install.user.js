50800de362315a687a7c65d1f84257a21608403b
Harrygiel <harrygiel@gmail.com>
2017-04-29 20:33:35 +0200

    TotalCons array correction

--- install.user.js
+++ install.user.js
@@ -5,17 +5,16 @@
 // @description  Automate HearthOfGalaxy things
 // @author       Harrygiel
 // @include      *
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/init/PRELOAD.js
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/init/LOADING.js
+// @exclude		 *.*
 // @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/init/INIT.js
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/lib/BRILLANDLIB.js
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/core/BRILLANDUPDATE.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/lib/BRILLIANDLIB.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/js/core/BRILLIANDUPDATE.js
 // @resource	 updateAutoRouteIcon https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev/img/updatehub.png
 // @grant        GM_getResourceURL
 // ==/UserScript==
 
 setTimeout(function(){ if(typeof game !== 'undefined' && typeof game.planets !== 'undefined'){
                           console.log("[SCRIPT] Script loaded !");
-                          load.loading();
+                          script.init();
                           }
                      }, 5000);
--- js/init/INIT.js
+++ js/init/INIT.js
@@ -1,4 +1,5 @@
 var script = new Object();
+
 script.init = function(){
 	/*
 	console.log(game.id);
@@ -6,10 +7,21 @@ script.init = function(){
 	console.dir(routes);
 	console.dir(fleetSchedule);
 	*/
+
+	//BRILLIAND VAR
+	hub = planetsName.santorini;
+	planetsTransportRes = Array(planets.length);
+	////////////////////////
+
 	var updateAutoRouteButton = document.createElement("img");
+	updateAutoRouteButton.id = "autobuildhub_icon";
+	updateAutoRouteButton.class = 'icon';
 	updateAutoRouteButton.src = GM_getResourceURL("updateAutoRouteIcon");
-	updateAutoRouteButton.setAttribute("height", "30");
-	updateAutoRouteButton.setAttribute("width", "30");
+	updateAutoRouteButton.height="30px";
+	updateAutoRouteButton.width="30px";
+	updateAutoRouteButton.style.top="6px";
+	updateAutoRouteButton.style.left="120px";
+	updateAutoRouteButton.style.cursor="pointer";
 	document.getElementById("icon_cont").appendChild(updateAutoRouteButton);
 
 	$("#autobuildhub_icon").click(function() {
@@ -17,6 +29,7 @@ script.init = function(){
         //script.UpdateAutoRoute();
         console.log("Updated !");
     });
+
     $("#autobuildhub_icon").hover(function() {
         (new m(120,
             20, "<span class='blue_text' style='width:100%;text-align:center'>Automaticaly correct<br>HUB routes</span>", "info")).drawInfo();
