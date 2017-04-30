e4b77b91e1f346e4615e3aa58098537525d9d087
Harrygiel <harrygiel@gmail.com>
2017-04-30 02:03:41 +0200

    Publish V 0.0.3
    Correction + cleaning

--- install.user.js
+++ install.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         HeartOfScript-Harrygiel
 // @namespace    https://github.com/Harrygiel/HeartOfGalaxyScript/tree/master
-// @version      0.0.2
+// @version      0.0.3
 // @description  Automate HearthOfGalaxy things
 // @author       Harrygiel
 // @include      *
-// @require      http://www.kongregate.com/games/Cheslava/heart-of-galaxy
 // @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/init/INIT.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/lib/BRILLIANDLIB.js
 // @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/core/BRILLIANDUPDATE.js
 // @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/buttons/UPDATEAUTOROUTE.js
 // @resource	 updateAutoRouteIcon https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/img/updatehub.png
