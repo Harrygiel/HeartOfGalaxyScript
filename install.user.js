a9f82c4a9ff103bb130f718d64d289745f735e0c
Harrygiel <harrygiel@gmail.com>
2017-05-03 00:22:03 +0200

    Publish v0.1.0
    See Changelog
    
    * Correcting an error in the popup calculator
    * Cleaned useless file
    * Updated install.user.js
    * Updated Changelog
    * Updated README.md
    	warning summary added
    	feature added
    	"updating the script" added
    	Changelog added

--- install.user.js
+++ install.user.js
@@ -1,23 +1,23 @@
 // ==UserScript==
 // @name         [DEV]HeartOfScript-Harrygiel
-// @namespace    https://github.com/Harrygiel/HeartOfGalaxyScript/tree/dev-Harrygiel
+// @namespace    https://github.com/Harrygiel/HeartOfGalaxyScript/tree/master
 // @version      0.1.0
 // @description  Automate HearthOfGalaxy things
 // @author       Harrygiel
 // @match        https://game274411.konggames.com/gamez/0027/4411/live/*
 //////////////////////////////////////////////////////////////////////////
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/lib/GAMELIB.js
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/lib/BRILLIANDLIB.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/lib/GAMELIB.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/lib/BRILLIANDLIB.js
 //
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/core/UPDATEAUTOROUTECORE.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/core/UPDATEAUTOROUTECORE.js
 //
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/ui/UPDATEAUTOROUTEUIMANAGER.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/ui/UPDATEAUTOROUTEUIMANAGER.js
 //
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/buttons/UPDATEAUTOROUTEBUTTON.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/buttons/UPDATEAUTOROUTEBUTTON.js
 //
-// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/js/init/INIT.js
+// @require      https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/js/init/INIT.js
 //////////////////////////////////////////////////////////////////////////
-// @resource	 updateAutoRouteIcon https://rawgit.com/Harrygiel/HeartOfGalaxyScript/dev-Harrygiel/img/updatehub.png
+// @resource	 updateAutoRouteIcon https://rawgit.com/Harrygiel/HeartOfGalaxyScript/master/img/updatehub.png
 //////////////////////////////////////////////////////////////////////////
 // @grant        GM_getResourceURL
 // ==/UserScript==
